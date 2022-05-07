211bb1d857db9d0515bd7075cd4dac5d610a6d18
Eric Rasolofonirina <ericseven.mg@gmail.com>
2022-05-08 00:07:01 +0300

    Generate code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,49 @@
-const codes = document.querySelectorAll('.code')
+const generate = document.getElementById('generate')
+const number1 = document.getElementById('number1')
+const number2 = document.getElementById('number2')
+const number3 = document.getElementById('number3')
+const number4 = document.getElementById('number4')
+const pinCode = []
 
-codes[0].focus()
+generateCode()
 
-codes.forEach((code, index) => {
-    code.addEventListener('keydown', (e) => {
-        if(e.key >= 0 && e.key <=9) {
-            codes[index].value = ''
-            setTimeout(() => codes[index + 1].focus(), 10)
-        } else if(e.key === 'Backspace') {
-            setTimeout(() => codes[index - 1].focus(), 10)
-        }
-    })
+generate.addEventListener('click', () => {
+    while(pinCode.length > 0) {
+        pinCode.pop();
+    }
+    generateCode()
 })
+
+function generateCode() {
+    let num1 = generateRandomNumber()
+    pinCode.push(num1)
+
+    let num2 = generateRandomNumber()
+    num2 = compare(num1, num2)
+    pinCode.push(num2)
+
+    let num3 = generateRandomNumber()
+    num3 = compare(num2, num3)
+    pinCode.push(num3)
+
+    let num4 = generateRandomNumber()
+    num4 = compare(num3, num4)
+    pinCode.push(num4)
+
+    number1.value = pinCode[0]
+    number2.value = pinCode[1]
+    number3.value = pinCode[2]
+    number4.value = pinCode[3]
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10)
+}
+
+function compare(a, b) {
+    if (a === b) {
+        return (b === 9) ? 0 : (b + 1)
+    } else {
+        return b
+    }
+}
